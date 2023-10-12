02156395d5bc0977c5976eca6c90470c78f51532
Tanmayee12345 <tanutanmayee2003@gmail.com>
2023-10-12 23:25:04 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+import Age from './component/age';
+function App() {
+  return (
+   <div>
+ <Age/>
+   </div>
+  );
+}
+
+export default App;
+// echo "# Reacttask5" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Tanmayee12345/Reacttask5.git
+// git push -u origin main
